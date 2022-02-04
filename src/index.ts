5312243f8ce591f33343d946a2ea304f4cfef9fa
masaya.suzuki <suzukimasaya428@gmail.com>
2022-02-05 08:34:57 +0900

    eslintとprettierの設定変更

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { startVideoNotify } from './youtube'
-import { startSlack } from './slack'
-import { postgresConnect, postgresDisconnect } from './db'
+import { startVideoNotify } from "./youtube";
+import { startSlack } from "./slack";
+import { postgresConnect, postgresDisconnect } from "./db";
 
-async function main (): Promise<void> {
-  await postgresConnect()
-  await startSlack()
-  await startVideoNotify()
-  await postgresDisconnect()
+async function main(): Promise<void> {
+  await postgresConnect();
+  await startSlack();
+  await startVideoNotify();
+  await postgresDisconnect();
 }
 
-main()
+main();
