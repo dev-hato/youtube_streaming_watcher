6e479c440ba474e6282f74d4eda0a4f685de6b2a
masaya.suzuki <suzukimasaya428@gmail.com>
2022-02-23 20:43:25 +0900

    ルール無効化

--- src/common/slack.ts
+++ src/common/slack.ts
@@ -56,7 +56,7 @@ async function getChannelData (
   message: GenericMessageEvent,
   say: SayFn
 ): Promise<RegisteredChannel | undefined> {
-  let id = message.text?.split(/[  ]/)[2]
+  let id = message.text?.split(/[  ]/)[2] // eslint-disable-line no-irregular-whitespace
     .replace(/</g, '')
     .replace(/>/g, '')
     .replace(/https:\/\/www\.youtube\.com\/channel\//g, '')
