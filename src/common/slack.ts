8e78de58bfa89b3113fb76d90034d4f64ba7f303
masaya.suzuki <suzukimasaya428@gmail.com>
2022-02-23 20:39:24 +0900

    Slackのコマンドをコピペで入力できるようにする

--- src/common/slack.ts
+++ src/common/slack.ts
@@ -56,7 +56,7 @@ async function getChannelData (
   message: GenericMessageEvent,
   say: SayFn
 ): Promise<RegisteredChannel | undefined> {
-  let id = message.text?.split(' ')[2]
+  let id = message.text?.split(/[  ]/)[2]
     .replace(/</g, '')
     .replace(/>/g, '')
     .replace(/https:\/\/www\.youtube\.com\/channel\//g, '')
