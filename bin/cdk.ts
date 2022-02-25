3fdec66731ef0e7481be9f3cae87222f1e490a57
masaya.suzuki <suzukimasaya428@gmail.com>
2022-02-25 09:27:39 +0900

    AWS CDK v2へアップデート

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core'
+import { App } from 'aws-cdk-lib'
 import { CdkStack } from '../lib/cdk-stack'
 
-const app = new cdk.App()
+const app = new App()
 new CdkStack(app, 'Stack-youtube-streaming-watcher') // eslint-disable-line no-new
--- lib/props/function-props.ts
+++ lib/props/function-props.ts
@@ -1,15 +1,14 @@
-import * as cdk from '@aws-cdk/core'
-import { NodejsFunctionProps } from '@aws-cdk/aws-lambda-nodejs'
+import { Duration, aws_lambda_nodejs as lambdaNode } from 'aws-cdk-lib'
 
-export const functionProps: { [key: string]: NodejsFunctionProps } = {
+export const functionProps: { [key: string]: lambdaNode.NodejsFunctionProps } = {
   notify: {
     functionName: 'youtube_streaming_watcher_notify_function',
     entry: 'src/notify/index.ts',
-    timeout: cdk.Duration.minutes(5)
+    timeout: Duration.minutes(5)
   },
   reply: {
     functionName: 'youtube_streaming_watcher_reply_function',
     entry: 'src/reply/index.ts',
-    timeout: cdk.Duration.seconds(10)
+    timeout: Duration.seconds(10)
   }
 }
