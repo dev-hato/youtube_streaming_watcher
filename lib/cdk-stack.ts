0bb2a70f90bab667426ed448fe386c8911b227c4
masaya.suzuki <suzukimasaya428@gmail.com>
2022-02-28 00:57:24 +0900

    デプロイ用IAMロール名をIAMロールから取得

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -194,7 +194,7 @@ export class CdkStack extends Stack {
       ]
     })
 
-    const cdkDeployRoleName = 'youtube_streaming_watcher_cdk_deploy'
+    const cdkDeployRoleName = cdkRoles.deploy.roleName
     const iamRoleDeployPolicyResourceArns = [
       functionData.notify.role?.roleArn,
       functionData.reply.role?.roleArn,
