d580a2482be72db484cea2a50e9b6f9d89d28105
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-04 00:48:15 +0900

    処理をまとめる

--- scripts/npm_ci.sh
+++ scripts/npm_ci.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-npm_version=$(jq -r '.engines.npm | ltrimstr("^")' package.json)
-npm install --prefer-offline --location=global "npm@${npm_version}"
+bash "${GITHUB_WORKSPACE}/scripts/install_npm.sh"
 npm ci --prefer-offline
