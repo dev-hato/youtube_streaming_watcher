2af3153fa352aa87d946cde32aee743455e1fcff
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-28 22:28:12 +0900

    clean_cache修正

--- scripts/clean_cache/clean_cache/clean-cache.js
+++ scripts/clean_cache/clean_cache/clean-cache.js
@@ -1,36 +1,24 @@
 module.exports = async ({ github, context }) => {
-  while (true) {
-    const actionsGetActionsCacheUsageParams = {
-      owner: context.repo.owner,
-      repo: context.repo.repo
-    }
-    console.log('call actions.getActionsCacheUsage', actionsGetActionsCacheUsageParams)
-    const actionsCacheUsage = await github.rest.actions.getActionsCacheUsage(actionsGetActionsCacheUsageParams)
+  const actionsGetActionsCacheListParams = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    sort: 'last_accessed_at',
+    direction: 'asc'
+  }
+  console.log('call actions.getActionsCacheList', actionsGetActionsCacheListParams)
+  const actionsGetActionsCacheList = await github.paginate(github.rest.actions.getActionsCacheList, actionsGetActionsCacheListParams)
+  const actionCaches = actionsGetActionsCacheList.actions_caches
+  let sumSize = actionCaches.reduce((sum, size) => sum + (size.size_in_bytes ?? 0), 0)
 
-    if (actionsCacheUsage.data.active_caches_size_in_bytes <= 9 * 1024 * 1024 * 1024) {
-      return
-    }
-
-    const actionsGetActionsCacheListParams = {
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      sort: 'last_accessed_at',
-      direction: 'asc'
-    }
-    console.log('call actions.getActionsCacheList', actionsGetActionsCacheListParams)
-    const actionsGetActionsCacheList = await github.rest.actions.getActionsCacheList(actionsGetActionsCacheListParams)
-    const actionCaches = actionsGetActionsCacheList.data.actions_caches
-
-    if (actionCaches.length < 1) {
-      return
-    }
-
+  while (8 * 1024 * 1024 * 1024 < sumSize) {
+    const actionCache = actionCaches.shift()
     const actionsDeleteActionsCacheByKey = {
       owner: context.repo.owner,
       repo: context.repo.repo,
-      key: actionCaches[0].key
+      key: actionCache.key
     }
     console.log('call actions.deleteActionsCacheByKey', actionsDeleteActionsCacheByKey)
     await github.rest.actions.deleteActionsCacheByKey(actionsDeleteActionsCacheByKey)
+    sumSize -= actionCache.size_in_bytes
   }
 }
