cd280c4d75db752b3455bc0bad53af298380637c
masaya.suzuki <suzukimasaya428@gmail.com>
2022-12-09 22:47:08 +0900

    パス修正

--- src/notify/index.dev.ts
+++ src/notify/index.dev.ts
@@ -2,7 +2,7 @@ import sleep from 'sleep-promise'
 import express from 'express'
 import { handler } from './index'
 import { createTables } from '../common/dynamodb.dev'
-import { rate } from '../../lib/props/events-rule-props'
+import { rate } from '../../lib/default/props/events-rule-props'
 import { pingHandler, pingPath, port } from '../common/server.dev'
 
 async function devHandler (): Promise<void> {
